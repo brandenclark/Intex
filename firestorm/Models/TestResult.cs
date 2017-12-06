7eda5fc402399d9217c148446ffe44c05da7d979
Patrik Drean <drean1@byu.edu>
2017-12-06 16:47:30 -0700

    TestResult

--- firestorm/Models/TestResult.cs
+++ firestorm/Models/TestResult.cs
@@ -7,7 +7,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace firestorm.Models
 {
+    [Table("TestResult")]
     public class TestResult
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Key, Column(Order = 0)]
+        [ForeignKey("SampleTest", Column(Order = 0))]
+        public int TestTubeID { get; set; }
+        public virtual SampleTest SampleTest { get; set; }
+
+        [Key, Column(Order = 1)]
+        [ForeignKey("TestReport", Column(Order = 1))]
+        public int ReportTypeCode { get; set; }
+        public virtual TestReport TestReport { get; set; }
+
+        public string FilePath { get; set; }
+
     }
 }
