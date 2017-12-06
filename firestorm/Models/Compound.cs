2d3a1c3db608d0e5541dc1bd6bdb1c47f4833fa0
Branden Clark <brandenrayclark@gmail.com>
2017-12-06 15:34:19 -0700

    compound model

--- firestorm/Models/Compound.cs
+++ firestorm/Models/Compound.cs
@@ -7,7 +7,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace firestorm.Models
 {
+    [Table("Compound")]
     public class Compound
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int LT { get; set; }
+        public String Name { get; set; }
+
     }
 }
