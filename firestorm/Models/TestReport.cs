326745bfed6f9c20e0f6d31795893a592a61a641
Kei Okada <keiokada1992@gmail.com>
2017-12-06 16:40:42 -0700

    TestReport

--- firestorm/Models/TestReport.cs
+++ firestorm/Models/TestReport.cs
@@ -7,7 +7,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace firestorm.Models
 {
+    [Table("TestReport")]
     public class TestReport
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ReportTypeCode { get; set; }
+        public String Descriptions { get; set; }
+
     }
 }
