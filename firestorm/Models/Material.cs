b6a91c0205c034eaaa79256dcccb0c3c2736de7d
Kei Okada <keiokada1992@gmail.com>
2017-12-06 15:27:36 -0700

    Material

--- firestorm/Models/Material.cs
+++ firestorm/Models/Material.cs
@@ -7,7 +7,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace firestorm.Models
 {
+    [Table("Material")]
     public class Material
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int MaterialID { get; set; }
+        public String MaterialName { get; set; }
+        public decimal MaterialPrice { get; set; }
     }
 }
