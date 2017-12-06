b18deb80a225599f78b806f14980083bf3017129
Patrik Drean <drean1@byu.edu>
2017-12-06 16:02:34 -0700

    Invoice Model

--- firestorm/Models/Invoice.cs
+++ firestorm/Models/Invoice.cs
@@ -7,7 +7,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace firestorm.Models
 {
+    [Table("Invoice")]
     public class Invoice
     {
+        [Key]
+        public int InvoiceID { get; set; }
+        public DateTime DateDue { get; set; }
+        public DateTime EarlyPayDate { get; set; }
+        public decimal EarlyPayDiscount { get; set; }
+        public decimal Amount { get; set; }
+
+        [ForeignKey("WorkOrder")]
+        public virtual int OrderID { get; set; }
+        public virtual WorkOrder WorkOrder { get; set; }
+
+        [ForeignKey("Company")]
+        public virtual int CompanyID { get; set; }
+        public virtual Company Company { get; set; }
+
     }
 }
