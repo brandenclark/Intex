84a5d18a8135dacf1a83b93db640efd6a222d6d1
Patrik Drean <drean1@byu.edu>
2017-12-06 16:06:05 -0700

    DataReport

--- firestorm/Models/DataReport.cs
+++ firestorm/Models/DataReport.cs
@@ -7,7 +7,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace firestorm.Models
 {
+    [Table("DataReport")]
     public class DataReport
     {
+        [Key]
+        public int DataRptID { get; set; }
+        public string FilePath { get; set; }
+
+        [ForeignKey("SampleTest")]
+        public virtual int TestTubeID { get; set; }
+        public virtual SampleTest SampleTest { get; set; }
     }
 }
