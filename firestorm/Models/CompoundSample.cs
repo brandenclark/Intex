96e4247b013130c1dd86acb729ced05372227dde
Branden Clark <brandenrayclark@gmail.com>
2017-12-06 15:55:59 -0700

    compoundsample model

--- firestorm/Models/CompoundSample.cs
+++ firestorm/Models/CompoundSample.cs
@@ -2,12 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using firestorm.Models;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace firestorm.Models
 {
+    [Table("CompoundSample")]
     public class CompoundSample
     {
+        [Key]
+        [Column(Order=1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [ForeignKey("Compound")]
+        public int LT { get; set; }
+        public virtual Compound compound { get; set; }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int SequenceCode { get; set; }
+
+        public int QuantityMG { get; set; }
+        public DateTime DateArrived { get; set; }
+        public String ReceivedBy { get; set; }
+        public DateTime DateDue { get; set; }
+        public String Appearance { get; set; }
+        public int Weight { get; set; }
+        public float MolMass { get; set; }
+        public bool AuthAddTest { get; set; }
+        public DateTime ScheduledDate { get; set; }
+
+        [ForeignKey("Assay")]
+        public String AssayID { get; set; }
+        public virtual Assay Assay { get; set; }
+
+        //Add WorkOrder
+
+
+
     }
 }
