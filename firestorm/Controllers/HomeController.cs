4cbc008537a821d0f217ff2818dc21126cbdbcdf
Patrik Drean <drean1@byu.edu>
2017-12-07 08:05:32 -0700

    Customer Controller Update

--- firestorm/Controllers/HomeController.cs
+++ firestorm/Controllers/HomeController.cs
@@ -15,8 +15,7 @@ namespace firestorm.Controllers
     public class HomeController : Controller
     {
         private Thunderstorm db = new Thunderstorm();
-
-        [Authorize]
+        static User PendedUser = new User();
         public ActionResult Index()
         {
             return View();
@@ -51,5 +50,86 @@ namespace firestorm.Controllers
                 return View();
             }
         }
+
+
+        public ActionResult CreateAccount()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateAccount([Bind(Include = "UserID,FirstName,LastName,Phone,Email,Password,RoleID,CompanyID")] User user, FormCollection form)
+        {
+            if (ModelState.IsValid)
+            {
+                // Assign user to their role
+                user.RoleID = 1;
+
+                // Assign PK
+                user.UserID = db.Users.ToList().Max(x => x.UserID) + 1;
+
+                // Determine if company is already in db
+                List <Company> Companies = db.Companies.ToList();
+                Boolean CompanyExists = false;
+
+                foreach(var Item in Companies)
+                {
+                    if (Item.Name.Equals(form["CompanyName"].ToString()))
+                    {
+                        user.CompanyID = Item.CompanyID;
+                        CompanyExists = true;
+                    }
+                }
+
+                // Add custoemr to the database if the company exists
+                if (CompanyExists)
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                else
+                {
+                    PendedUser = user;
+                    return RedirectToAction("CreateCompany");
+                }
+            }
+
+            return View(user);
+        }
+
+        public ActionResult CreateCompany()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateCompany([Bind(Include = "CompanyID, Name, Address, City, State, Zip, Balance")] Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                // Assign company's pk
+                company.CompanyID = db.Companies.Max(x => x.CompanyID) + 1;
+
+                // Give 0 balance
+                company.Balance = 0;
+
+                // Assign the user the company id
+                PendedUser.CompanyID = company.CompanyID;
+
+                // Update db with new company and user
+                db.Companies.Add(company);
+                db.Users.Add(PendedUser);
+               
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+            return View(company);
+        }
+
+
     }
 }
---
+++ firestorm/Models/WorkOrderCompoundSample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace firestorm.Models
+{
+    public class WorkOrderCompoundSample
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public WorkOrder workOrder { get; set; }
+        public List <CompoundSample> compoundSamples { get; set; }
+
+    }
+}
