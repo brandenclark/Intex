5d96db45e22e37c92c80de0156ad1dff1c5171fa
Branden Clark <brandenrayclark@gmail.com>
2017-12-07 09:25:01 -0700

    Summary Report init

--- firestorm/Controllers/ManagementController.cs
+++ firestorm/Controllers/ManagementController.cs
@@ -25,13 +25,7 @@ namespace firestorm.Controllers
 
         public ActionResult SummaryReport()
         {
-            var SumRpts = db.Database.SqlQuery<String>("SELECT wo.OrderID FROM SummaryReport sr RIGHT JOIN WorkOrder wo ON sr.OrderID = wo.OrderID "
-                                    + "INNER JOIN CompoundSample cs ON cs.OrderID = wo.OrderID "
-                                    + "INNER JOIN SampleTest st ON st.LT = cs.LT AND st.SequenceCode = cs.SequenceCode "
-                                    + "WHERE st.CompletedTest = 1 AND wo.OrderID NOT IN("
-                                    + "SELECT wo.OrderID FROM WorkOrder wo INNER JOIN CompoundSample cs ON cs.OrderID = wo.OrderID"
-                                    + " INNER JOIN SampleTest st ON st.LT = cs.LT AND st.SequenceCode = cs.SequenceCode"
-                                    + "WHERE st.CompletedTest = 0) AND sr.OrderID IS NULL GROUP BY wo.OrderID").ToList();
+            var SumRpts = db.Database.SqlQuery<int>("SELECT wo.OrderID FROM SummaryReport sr RIGHT JOIN WorkOrder wo ON sr.OrderID = wo.OrderID INNER JOIN CompoundSample cs ON cs.OrderID = wo.OrderID  INNER JOIN SampleTest st ON st.LT = cs.LT AND st.SequenceCode = cs.SequenceCode WHERE st.CompletedTest = 1 AND wo.OrderID NOT IN(SELECT wo.OrderID FROM WorkOrder wo INNER JOIN CompoundSample cs ON cs.OrderID = wo.OrderID INNER JOIN SampleTest st ON st.LT = cs.LT AND st.SequenceCode = cs.SequenceCode WHERE st.CompletedTest = 0) AND sr.OrderID IS NULL GROUP BY wo.OrderID").ToList();
 
             return View(SumRpts);
         }
