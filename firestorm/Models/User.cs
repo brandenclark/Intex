74bc77a880a361473f4bb0769a594549ecf7c36c
Patrik Drean <drean1@byu.edu>
2017-12-07 22:15:22 -0700

    p

--- firestorm/Models/User.cs
+++ firestorm/Models/User.cs
@@ -14,20 +14,33 @@ namespace firestorm.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int UserID { get; set; }
+
+        [Required(ErrorMessage ="Please enter your first name")]
         [DisplayName("First Name")]
         public String FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
         [DisplayName("Last Name")]
         public String LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        [RegularExpression(@"\(?\d{3}\)?-? *\d{3}-? *-?\d{4}", ErrorMessage = "Please enter " +
+                            " phone number as (xxx) xxx-xxxx")]
         public String Phone { get; set; }
         [EmailAddress]
+
+        [Required(ErrorMessage = "Please enter your email address")]
         public String Email { get; set; }
-        [PasswordPropertyText]
+
+        [Required(ErrorMessage = "Please enter a password")]
+        [DataType(DataType.Password)]
         public String Password { get; set; }
         
         [ForeignKey("Role")]
         public virtual int? RoleID { get; set; }
         public virtual Role Role { get; set; }
 
+        [Required(ErrorMessage = "Please enter your company")]
         [ForeignKey("Company")]
         public virtual int? CompanyID { get; set; }
         public virtual Company Company { get; set; }
